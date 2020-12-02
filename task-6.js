2d78b9935bbb4356416fed3f6b20cf415e9ea5da
InnaKovshik <kovshikinna@gmail.com>
2020-12-02 12:10:39 +0200

    all

---
+++ task-6.js
@@ -0,0 +1,11 @@
+let input = +prompt("введи число");
+let total = 0;
+while(input){
+  total += input;
+  input = +prompt("введи число");  
+}; 
+alert("Общая сумма чисел равна ["+ total +"]");
+
+console.log(input);
+
+
